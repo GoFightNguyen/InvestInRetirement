9dab42ddeb7e533391631e1ed02a0ee21eb8f0a3
GoFightNguyen <gofightnguyen@hotmail.com>
2019-03-27 18:31:52 -0500

    Automating the specs for How Much I Invest...not quite happy but they are in place and passing

---
+++ tests/Domain.Specs/HowMuchIInvest.feature
@@ -0,0 +1,25 @@
+﻿Feature: How much I invest
+	Given my annual salary
+	When I invest a percentage of it
+	Then I should be told the dollar amount I am investing
+
+Scenario Outline: Single Investment
+	Given my annual salary is <annualSalary>
+	When I invest <percentage>% into an investment
+	Then I am investing <moniesInvested>
+	Examples:
+		| annualSalary | percentage | moniesInvested |
+		| $106,156     | 6          | $6,369.36      |
+		| $106,156     | 5.18       | $5,498.88      |
+		| $24,136.25   | 16         | $3,861.80      |
+
+Scenario: Multiple Investments
+	Given my annual salary is $106,156
+	When I invest
+	| percentage | investment  |
+	| 6          | 401(k)      |
+	| 5.18       | ROTH 401(k) |
+	Then I am investing
+	| moniesInvested | investment  |
+	| $6,369.36      | 401(k)      |
+	| $5,,498.88     | ROTH 401(k) |
--- tests/Domain.Specs/Steps.cs
+++ tests/Domain.Specs/Steps.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using TechTalk.SpecFlow;
 
 namespace Domain.Specs
@@ -7,7 +8,9 @@ namespace Domain.Specs
     public class Steps
     {
         private decimal _annualSalary;
+        private Percent _desiredInvestmentPercentage;
         private Percent _investmentPercentage;
+        private Dictionary<string, Percent> _investmentPercentages = new Dictionary<string, Percent>();
 
         [Given(@"my annual salary is \$(.*)")]
         public void GivenMyAnnualSalaryIs(decimal annualSalary)
@@ -18,14 +21,48 @@ namespace Domain.Specs
         [When(@"I want to invest (.*)% into retirement")]
         public void WhenIWantToInvestIntoRetirement(decimal percentage)
         {
-            _investmentPercentage = percentage;
+            _desiredInvestmentPercentage = percentage;
+        }
+
+        [When(@"I invest (.*)% into an investment")]
+        public void WhenIInvestIntoAn(decimal percentange)
+        {
+            _investmentPercentage = percentange;
+        }
+
+        [When(@"I invest")]
+        public void WhenIInvest(Table table)
+        {
+            foreach (var row in table.Rows)
+            {
+                _investmentPercentages.Add(row["investment"], decimal.Parse(row["percentage"]));
+            }
         }
 
         [Then(@"I am told to invest \$(.*)")]
         public void ThenIAmToldToInvest(decimal expected)
         {
-            var moniesToInvest = _investmentPercentage.Of(_annualSalary);
+            var moniesToInvest = _desiredInvestmentPercentage.Of(_annualSalary);
             Assert.AreEqual(expected, moniesToInvest);
+        }
+
+        [Then(@"I am investing \$(.*)")]
+        public void ThenIAmInvesting(decimal expected)
+        {
+            var moniesInvested = _investmentPercentage.Of(_annualSalary);
+            Assert.AreEqual(expected, moniesInvested);
+        }
+
+        [Then(@"I am investing")]
+        public void ThenIAmInvesting(Table table)
+        {
+            foreach (var row in table.Rows)
+            {
+                var investment = row["investment"];
+                var expected = decimal.Parse(row["moniesInvested"], System.Globalization.NumberStyles.Currency);
+                var actual = _investmentPercentages[investment].Of(_annualSalary);
+                Assert.AreEqual(expected, actual, investment);
+            }
         }
     }
 }
